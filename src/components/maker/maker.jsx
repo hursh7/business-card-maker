0dd9a1045ad842b1a88e126e183dabadca38c7f1
junhp7 <junhp1994@gmail.com>
2021-09-08 13:40:01 +0900

    add card and edit from components

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -1,10 +1,47 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../header/header';
 import Footer from '../footer/footer';
+import Editor from '../editor/editor';
+import Preview from '../preview/preview';
 import styles from './maker.module.css';
 import { useHistory } from 'react-router';
 
 const Maker = ({ authService }) => {
+    const [cards, setCards] = useState([
+        {
+            id: '1',
+            name: 'jun',
+            company: 'Kakao',
+            theme: 'dark',
+            title: 'FrontEnd Developer',
+            email: 'junhp7@naver.com',
+            message: 'go for it',
+            fileName: 'fileName',
+            fileURL: null
+        },
+        {
+            id: '2',
+            name: 'jun2',
+            company: 'Kakao',
+            theme: 'light',
+            title: 'FrontEnd Developer',
+            email: 'junhp7@naver.com',
+            message: 'go for it',
+            fileName: 'fileName',
+            fileURL: null
+        },
+        {
+            id: '3',
+            name: 'jun3',
+            company: 'Kakao',
+            theme: 'colorful',
+            title: 'FrontEnd Developer',
+            email: 'junhp7@naver.com',
+            message: 'go for it',
+            fileName: 'fileName',
+            fileURL: null
+        }
+    ]);
     const history = useHistory();
     const onLogout = () => {
         authService.logout();
@@ -21,6 +58,10 @@ const Maker = ({ authService }) => {
     return (
         <section className={styles.maker}>
             <Header onLogout={onLogout} />
+            <div className={styles.container}>
+                <Editor cards = {cards} />
+                <Preview cards = {cards} />
+            </div>
             <Footer />
         </section>
     )
